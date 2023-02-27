ea4084e6b105aa6b5fc401c1b8e986df43b5adaa
SamCcode <115998847+SamCcode@users.noreply.github.com>
2023-02-27 17:19:45 +0100

    added counters

--- puzzle.js
+++ puzzle.js
@@ -1,24 +1,51 @@
 
+// vIUnsk19PCR83ZL0lszKpw0e2csjJ_vw6e0dmoMlbeQ acceskey
+// Bfo-8nNYkkbnQsBr5kuNRWy60BgfREfEfg-Gjskp2eE secretkey
+
 let canvasWrapper = document.querySelector(".canvaswrapper")
 let imgWrapper = document.querySelector(".imgwrapper")
 let squares = document.querySelectorAll(".squares")
+let body = document.querySelector("body");
 let canvasPieces = [];
 let emptySquareList = [];
-let image = new Image();
-image.onload = cutImageUp;
-image.src = "bild.png";
+// image.onload = cutImageUp;
+// let image = new Image();
+// image.onload = cutImageUp;
+// image.src = "/bild.png";
 let rows = 3;
 let columns = 3;
 let canvasEl;
 let elem;
 let num1;
 let num2;
+let rightPiecesCounter = 0;
+let attemptsCounter = 15;
+let wonGamesCounter = 0;
+let lostGamesCounter = 0;
+let playedGamesCounter = 0;
+// getImgFromApi()
+startGame()
+async function getImgFromApi() {
+    let randomImg = await fetch( "https://api.unsplash.com/photos/random/?client_id=vIUnsk19PCR83ZL0lszKpw0e2csjJ_vw6e0dmoMlbeQ")
+    randomImg = await randomImg.json();
+    console.log(randomImg);
+    randomImg = await randomImg.links.download;
+}
+
 
 // för att dela bilden
+function startGame() {
+    canvasPieces = [];
+    emptySquareList = [];
+    image = new Image();
+    image.onload = cutImageUp;
+    image.src = "/bild.png";
+}
+
 function cutImageUp() {
     for(var x = 0; x < columns; ++x) {
         for(var y = 0; y < rows; ++y) {
-            canvasEl = document.createElement('canvas');
+            canvasEl = document.createElement("canvas");
             canvasEl.classList.add("pieces");
             canvasEl.setAttribute("draggable", "true")
             canvasEl.style.width = "70px";
@@ -39,6 +66,7 @@ addEmptySquares()
 
 // för att göra tomma rutor att dra bitarna till
 function addEmptySquares(){
+    imgWrapper.innerHTML = "";
     for (let i = 0; i<canvasPieces.length; i++) {
         let emptySquare = document.createElement("div");
         emptySquare.classList.add("squares");
@@ -56,7 +84,7 @@ function addEventListnersToPieces(canvasEl) {
         num2 = canvasPieces.indexOf(elem);
         localStorage.setItem("dragedPiece", elem.innerHTML);
         setTimeout(() => {
-            canvasEl.style.display = "none";
+            elem.style.opacity = "0.5";
         }, 0);
         
     })
@@ -64,10 +92,11 @@ function addEventListnersToPieces(canvasEl) {
 
 // för att sätta eventlyssnare under tiden man drar
 function addEventListnersToWrapper(){
-     emptySquareList.forEach(emptySquare => {
+    emptySquareList.forEach(emptySquare => {
         emptySquare.addEventListener('dragenter', (e) =>{
+            console.log(elem);
             e.preventDefault();
-            num1 = emptySquareList.indexOf(emptySquare)
+            num1 = emptySquareList.indexOf(emptySquare);
         })
         emptySquare.addEventListener('dragover', (e)=> {
             e.preventDefault();
@@ -79,21 +108,74 @@ function addEventListnersToWrapper(){
             drop(e)
         });
     });
+    body.addEventListener("ondrop", (e) =>{
+        console.log(e);
+    })
 }
 
 // vad som ska hända när rätt ruta släpps på rätt nummer
 function drop(e) {
     if (num1 === 0 && num2 === 0 || num1 === 1 && num2 === 3 || num1 === 2 && num2 === 6 ||num1 === 3 && num2 === 1 || num1 === 4 && num2 === 4 || num1 === 5 && num2 === 7 || num1 === 6 && num2 === 2 || num1 === 7 && num2 === 5 || num1 === 8 && num2 === 8) {
-        console.log("hej");
         let draggedPiece = localStorage.getItem("dragedPiece");
         let addedPiece = document.createElement("div")
         addedPiece.innerHTML = `<img src="${draggedPiece}" style= "width:70px; height:70px;" alt="">`
         localStorage.clear();
         targetEl = e.target;
         targetEl.appendChild(addedPiece);
+        rightPiecesCounter++;
+        attemptsCounter--;
+        console.log(rightPiecesCounter);
+        gameIsFinished()
+        elem.style.display = "none";
+        console.log(elem);
     } else {
-        elem.style.display = "block";
+        attemptsCounter--;
+        gameIsFinished()
+        elem.style.opacity = "1";
     }
 }
 
 
+function gameIsFinished() {
+    let playedGames = document.querySelector(".played-games");
+    let wonGames = document.querySelector(".won-games");
+    let lostGames = document.querySelector(".lost-games");
+    let attempts = document.querySelector(".attempts");
+    let endGamePopUp = document.querySelector(".endgame-popup");
+    if (rightPiecesCounter >= 9) {
+        endGamePopUp.innerHTML = `
+        <p>Congratulations! You did it!</p>
+        <button class="play-again">Play again!</button>`
+        console.log("grattis");
+        wonGamesCounter++;
+        playedGamesCounter++;
+        let newGameBtn = document.querySelector(".play-again");
+        newGameBtn.addEventListener("click", () => {
+            endGamePopUp.innerHTML = "";
+            attemptsCounter = 15;
+            rightPiecesCounter = 0;
+            playedGames.innerHTML =`Played games: ${playedGamesCounter}`
+            wonGames.innerHTML = `Won games: ${wonGamesCounter}`
+            attempts.innerHTML = `Attempts left: ${attemptsCounter}`
+            startGame()
+        })
+    }
+    if (attemptsCounter === 0) {
+        endGamePopUp.innerHTML = `
+        <p>Sorry! You lost!</p>
+        <button class="play-again">Play again!</button>`
+        lostGamesCounter++
+        playedGamesCounter++
+        let newGameBtn = document.querySelector(".play-again");
+        newGameBtn.addEventListener("click", () => {
+            endGamePopUp.innerHTML = "";
+            rightPiecesCounter = 0;
+            attemptsCounter = 15;
+            playedGames.innerHTML =`Played games: ${playedGamesCounter}`
+            lostGames.innerHTML = `Lost games: ${lostGamesCounter}`
+            canvasWrapper.innerHTML = "";
+            startGame()
+        })
+    }
+    attempts.innerHTML = `Attempts left: ${attemptsCounter}`
+}
